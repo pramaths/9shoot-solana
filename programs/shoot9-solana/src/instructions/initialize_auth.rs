92e9912dffcb922eb3a32f230f436710c9f7ea0f
ritikbhatt20 <ritikbhatt020@gmail.com>
2025-03-05 05:14:36 +0000

    feat: added initilaize_auth

---
+++ programs/shoot9-solana/src/instructions/initialize_auth.rs
@@ -0,0 +1,35 @@
+use crate::state::AuthStore;
+use anchor_lang::prelude::*;
+
+#[derive(Accounts)]
+pub struct InitializeAuth<'info> {
+    #[account(mut)]
+    pub admin: Signer<'info>,
+    #[account(
+        init,
+        payer = admin,
+        space = 8 + 32 + (32 * 100),
+        seeds = [b"auth_store"],
+        bump
+    )]
+    pub auth_store: Account<'info, AuthStore>,
+    pub system_program: Program<'info, System>,
+}
+
+#[event]
+pub struct AuthInitialized {
+    pub admin: Pubkey,
+    pub timestamp: i64,
+}
+
+pub fn handler(ctx: Context<InitializeAuth>) -> Result<()> {
+    let auth_store = &mut ctx.accounts.auth_store;
+    auth_store.admin = ctx.accounts.admin.key();
+    auth_store.authorized_creators = Vec::new();
+
+    emit!(AuthInitialized {
+        admin: ctx.accounts.admin.key(),
+        timestamp: Clock::get()?.unix_timestamp,
+    });
+    Ok(())
+}
